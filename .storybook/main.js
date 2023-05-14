989fc371e9064f796043d90c463288a5ad7bbbfd
Luqman Olushi <olushilukmon03@gmail.com>
2023-05-14 14:47:38 +0100

    chore: config @storybook/addon-storysource

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,24 @@
 /** @type { import('@storybook/react-webpack5').StorybookConfig } */
+import path from "path";
+
 const config = {
   stories: ["../src/**/*.mdx", "../src/**/*.(stories|story).@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    {
+      name: "@storybook/addon-storysource",
+      options: {
+        rule: {
+          test: [/\.(stories|story)\.(js|jsx|ts|tsx)?$/],
+          include: [path.resolve(__dirname, "../src")]
+        },
+        loaderOptions: {
+          prettierConfig: { printWidth: 80, singleQuote: false }
+        }
+      }
+    }
   ],
   framework: {
     name: "@storybook/react-webpack5",
